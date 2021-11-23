85d5122b0f93849cf98513a815df727ab58d1785
Rosana Massahud <rosanamassahud@gmail.com>
2021-11-23 07:12:31 -0300

    add exercicio aula 1

---
+++ aula1/exercicioAula1.py
@@ -0,0 +1,12 @@
+# Fazer um algoritmo que calcula o fatorial de um número 'n'
+
+n = int(input("Digite um número: "))
+resultado = 1
+
+print("O fatorial de n é: {}! = {}".format(n,resultado))
+
+
+
+# Fazer um algoritmo para verificar se um número 'n' é primo
+
+n = int(input("Digite um número: "))
